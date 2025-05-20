de86476f62720dc446315dbe487e5b6dec8c95fe
FloSmt <109459476+FloSmt@users.noreply.github.com>
2025-05-20 13:07:08 +0200

    adjust requests

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,17 +1,17 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
+import {provideHttpClient, withFetch} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    importProvidersFrom(HttpClientModule)
+    provideHttpClient(withFetch()),
   ],
 };
